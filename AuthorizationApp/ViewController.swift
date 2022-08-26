2d18ee96e305c502307072a3f40cd2d6c9a18f62
Rigel <vadim_shink@icloud.com>
2022-08-26 13:01:21 +0300

    Initial Commit

---
+++ AuthorizationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuthorizationApp
+//
+//  Created by Vadim Shinkarenko on 26.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
